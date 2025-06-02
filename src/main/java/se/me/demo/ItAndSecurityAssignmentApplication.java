db64c0985da106333e74b25bf201856f2bff77ce
EricBO2 <eric.carlsson@stud.sti.se>
2025-06-02 15:41:14 +0200

    Inital commit

---
+++ src/main/java/se/me/demo/ItAndSecurityAssignmentApplication.java
@@ -0,0 +1,13 @@
+package se.me.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItAndSecurityAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItAndSecurityAssignmentApplication.class, args);
+    }
+
+}
